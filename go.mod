e9183f2f26b561bfcc0ef16a7e820c4a5d98bda1
Derek Kaplan <thederekkaplan@protonmail.com>
2020-06-24 22:19:40 -0400

    Reddit bot finalized
    Pulls events from next day from Mobilize
    Uses Reddit credentials stored in Secret Manager to xpost to r/YACUHQ

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/thederekkaplan/yacu-reddit-bot
 
 go 1.14
 
-require github.com/turnage/graw v0.0.0-20200617182045-4c520bf5831a // indirect
+require (
+	cloud.google.com/go v0.59.0
+	github.com/turnage/graw v0.0.0-20200617182045-4c520bf5831a
+	google.golang.org/genproto v0.0.0-20200624020401-64a14ca9d1ad
+)
--- main.go
+++ main.go
@@ -5,11 +5,37 @@ import (
 	"os"
 	"fmt"
 	"context"
+	"encoding/json"
+	"time"
+	"strconv"
+	"io/ioutil"
 
 	"github.com/turnage/graw/reddit"
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 )
+
+type MobilizeEvent struct {
+	Title string
+	Description string
+	Url string `json:"browser_url"`
+	Timeslots []Timeslot
+}
+
+type Timeslot struct {
+	Id float64
+	StartDate float64 `json:"start_date"`
+	EndDate float64 `json:"end_date"`
+}
+
+type Event struct {
+	Id float64
+	Title string
+	Description string
+	Url string
+	StartDate time.Time
+	EndDate time.Time
+}
 
 func main() {
 	http.HandleFunc("/update", update)
@@ -26,28 +52,120 @@ func main() {
 	}
 }
 
+func postEvents(account reddit.Account, events []Event) {
+	for _, event := range(events) {
+		account.PostLink(
+			"/r/YACUHQ",
+			"[" + event.StartDate.Format("Jan 2, 3:04 PM") + " Eastern] " + event.Title,
+			event.Url,
+		)
+	}
+}
+
 func update(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("X-Appengine-Cron") != true {
+	if r.Header.Get("X-Appengine-Cron") != "true" {
 		w.WriteHeader(http.StatusForbidden)
 		fmt.Fprint(w, "403 Forbidden")
 		return
 	}
 
 	cfg := reddit.BotConfig {
-		Agent: "golang:github.com/thederekkaplan/yacu-reddit-bot:v1.0.0"
-		App: app()
+		Agent: "golang:github.com/thederekkaplan/yacu-reddit-bot:v1.0.0",
+		App: app(),
+		Rate: 0,
 	}
 
+	bot, err := reddit.NewBot(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		panic(err)
+	}
+
+	now := time.Now().In(loc)
+	date := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	start := date.AddDate(0, 0, 1)
+	end := date.AddDate(0, 0, 2)
+
+	url := "https://api.mobilize.us/v1/organizations/2596/events?timeslot_start=gte_" +
+		strconv.Itoa(int(start.Unix())) + 
+		"&timeslot_start=lt_" +
+		strconv.Itoa(int(end.Unix()))
+
+	events := getEvents(url)
+
+	postEvents(bot, events)
 }
 
 func app() reddit.App {
-	projectID := "commanding-way-273100"
-
 	ctx := context.Background()
 	client, err := secretmanager.NewClient(ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	
+	accessReq := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: "projects/commanding-way-273100/secrets/reddit/versions/latest",
+	}
+
+	result, err := client.AccessSecretVersion(ctx, accessReq)
+	if err != nil {
+		panic(err)
+	}
+
+	var data reddit.App
+
+	err = json.Unmarshal(result.Payload.Data, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	return data
+}
+
+func getEvents(url string) []Event {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var objmap map[string]json.RawMessage
+	err = json.Unmarshal(body, &objmap)
+	if err != nil {
+		panic(err)
+	}
+
+	var mobilizeEvents []MobilizeEvent
+
+	err = json.Unmarshal(objmap["data"], &mobilizeEvents)
+	if err != nil {
+		panic(err)
+	}
+
+	var events []Event
+
+	// Convert timeslots into separate events
+	for _, event := range mobilizeEvents {
+		for _, timeslot := range event.Timeslots {
+			events = append(events, Event{
+				Id: timeslot.Id,
+				Title: event.Title,
+				Description: event.Description,
+				Url: event.Url,
+				StartDate: time.Unix(int64(timeslot.StartDate), 0),
+				EndDate: time.Unix(int64(timeslot.EndDate), 0),
+			})
+		}
+	}
+
+	return events
 }
